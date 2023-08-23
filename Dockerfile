adea56808e7a43d6d758eb80d1d976b80aa04f9d
buzhiyun <buzhiyun@sina.com>
2023-08-23 18:23:46 +0800

    update 1.24.0

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
-FROM nginx:1.22.1-alpine
+FROM nginx:1.24.0-alpine
 
 ADD nginx.conf /etc/nginx/nginx.conf
 ADD ali-* /usr/local/sbin/
 ADD docker-entrypoint.sh /
+#RUN rm /docker-entrypoint.d/30-tune-worker-processes.sh
 # 后续要增加的server站点
 # http项目的conf文件 映射到 /etc/nginx/conf.d 下
 # stream项目的conf文件 映射到 /etc/nginx/stream.d 下
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,59 +16,61 @@ function checkConf() {
                 md5check=`ls -lR /etc/nginx/ | md5sum`
             fi
         fi
-
     done  
 
 }
 
 
-if [ -z "${NGINX_ENTRYPOINT_QUIET_LOGS:-}" ]; then
-    exec 3>&1
-else
-    exec 3>/dev/null
-fi
-
-if [ "$1" = "nginx" -o "$1" = "nginx-debug" ]; then
-    if /usr/bin/find "/docker-entrypoint.d/" -mindepth 1 -maxdepth 1 -type f -print -quit 2>/dev/null | read v; then
-        echo >&3 "$0: /docker-entrypoint.d/ is not empty, will attempt to perform configuration"
-
-        echo >&3 "$0: Looking for shell scripts in /docker-entrypoint.d/"
-        find "/docker-entrypoint.d/" -follow -type f -print | sort -V | while read -r f; do
-            case "$f" in
-                *.sh)
-                    if [ -x "$f" ]; then
-                        echo >&3 "$0: Launching $f";
-                        "$f"
-                    else
-                        # warn on shell scripts without exec bit
-                        echo >&3 "$0: Ignoring $f, not executable";
-                    fi
-                    ;;
-                *) echo >&3 "$0: Ignoring $f";;
-            esac
-        done
-
-        echo >&3 "$0: Configuration complete; ready for start up"
-    else
-        echo >&3 "$0: No files found in /docker-entrypoint.d/, skipping configuration"
+entrypoint_log() {
+    if [ -z "${NGINX_ENTRYPOINT_QUIET_LOGS:-}" ]; then
+        echo "$@"
     fi
-fi
+}
+
+# 这一段在启动的时候不适用 CMD ["nginx"] 之类的参数，不会生效
+# 所以暂时先注释了
+# if [ "$1" = "nginx" -o "$1" = "nginx-debug" ]; then
+#     if /usr/bin/find "/docker-entrypoint.d/" -mindepth 1 -maxdepth 1 -type f -print -quit 2>/dev/null | read v; then
+#         entrypoint_log "$0: /docker-entrypoint.d/ is not empty, will attempt to perform configuration"
+# 
+#         entrypoint_log "$0: Looking for shell scripts in /docker-entrypoint.d/"
+#         find "/docker-entrypoint.d/" -follow -type f -print | sort -V | while read -r f; do
+#             case "$f" in
+#                 *.sh)
+#                     if [ -x "$f" ]; then
+#                         entrypoint_log "$0: Launching $f";
+#                         "$f"
+#                     else
+#                         # warn on shell scripts without exec bit
+#                         entrypoint_log "$0: Ignoring $f, not executable";
+#                     fi
+#                     ;;
+#                 *) entrypoint_log "$0: Ignoring $f";;
+#             esac
+#         done
+# 
+#         entrypoint_log "$0: Configuration complete; ready for start up"
+#     else
+#         entrypoint_log "$0: No files found in /docker-entrypoint.d/, skipping configuration"
+#     fi
+# fi
 
 checkConf &
 
 if [[ "$WORK_USER" != "nginx" ]]; then
-  echo try add user ${WORK_USER}
+  entrypoint_log try add user ${WORK_USER}
   if [ -z $WORK_USERID ];then
     adduser -D ${WORK_USER}
-    echo 'create user' ${WORK_USER}
+    entrypoint_log 'create user' ${WORK_USER}
   else
     adduser -D -u ${WORK_USERID} ${WORK_USER}
-    echo 'create user' ${WORK_USER} ',user id:'  ${WORK_USERID}
+    entrypoint_log 'create user' ${WORK_USER} ',user id:'  ${WORK_USERID}
   fi
 fi
 
-echo worker_processes ${WORK_PROCESSES} ", user" ${WORK_USER}
+entrypoint_log worker_processes ${WORK_PROCESSES} ", user" ${WORK_USER}
 #exec "nginx -g 'daemon off;worker_processes ${WORK_PROCESSES};'"
 nginx -g 'daemon off;worker_processes '${WORK_PROCESSES}';user '${WORK_USER}';'
+
 #exec "$@"
 
